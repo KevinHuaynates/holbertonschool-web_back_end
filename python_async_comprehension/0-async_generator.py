023ab443557d74102ca05293df9d26682412b4e9
KevinHuaynates <6992@holbertonstudents.com>
2024-01-29 08:15:51 -0800

    1

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,8 +2,9 @@
 """A courutine called async_generator that no take arguments"""
 import asyncio
 import random
+from typing import AsyncGenerator
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None, None]:
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
