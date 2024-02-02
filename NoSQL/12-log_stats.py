ed8b233029f60669263002b07fa6e791c0515188
KevinHuaynates <6992@holbertonstudents.com>
2024-02-01 23:33:51 -0800

    1

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,20 +1,33 @@
 #!/usr/bin/env python3
-"""Module Script that provides some stats about Nginx logs stored in MongoDB"""
+"""Module log_stats que devuelve status actual"""
 
 from pymongo import MongoClient
 
+def log_stats(logs_collection):
+    # Contar todos los documentos en la colección
+    total_logs = logs_collection.count_documents({})
+    
+    # Métodos a verificar
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    
+    # Inicializar un diccionario para almacenar el recuento de cada método
+    method_counts = {method: logs_collection.count_documents({"method": method}) for method in methods}
+    
+    # Contar los registros de status check
+    status_check_count = logs_collection.count_documents({"method": "GET", "path": "/status"})
+    
+    # Imprimir los resultados
+    print(f"{total_logs} logs")
+    print("Methods:")
+    for method in methods:
+        print(f"\tmethod {method}: {method_counts[method]}")
+    print(f"{status_check_count} status check")
+
 if __name__ == "__main__":
+    # Conectar a la base de datos y la colección
     client = MongoClient('mongodb://127.0.0.1:27017')
     logs_collection = client.logs.nginx
+    
+    # Llamar a la función log_stats con la colección como argumento
+    log_stats(logs_collection)
 
-    total_logs = logs_collection.count_documents({})
-    print(f"{total_logs} logs")
-
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        method_count = logs_collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {method_count}")
-
-    sstatus_check_filter = {"method": "GET", "path": "/status"}
-    status_check_count = logs_collection.count_documents(status_check_filter)
-    print(f"{status_check_count} status check")
