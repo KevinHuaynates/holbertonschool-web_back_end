4018a9d148b2a543285cab4bd80cb209cffd59db
KevinHuaynates <6992@holbertonstudents.com>
2024-02-04 14:59:03 -0800

    1

---
+++ pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+from typing import List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """Returns a dictionary with hypermedia pagination
+        """
+        assert index is None or (
+            isinstance(index, int) and 0 <= index < len(self.__indexed_dataset)
+        )
+        assert isinstance(page_size, int) and page_size > 0
+
+        data_page = []
+        next_index = None
+
+        if index is not None:
+            if index in self.__indexed_dataset:
+                start_index = index
+                end_index = index + page_size
+                indexes = range(start_index, end_index)
+                data_page = [self.__indexed_dataset[i] for i in indexes]
+                next_index = index + page_size
+
+        return {
+            'index': index,
+            'page_size': len(data_page),
+            'data': data_page,
+            'next_index': next_index
+        }
+
+
+# For testing purposes only when running this script directly
+if __name__ == "__main__":
+    # Deletion-resilient Hypermedia Pagination testing
+    server = Server()
+
+    try:
+        server.get_hyper_index(300000, 100)
+    except AssertionError:
+        print("AssertionError raised when out of range")
+
+    index = 3
+    page_size = 2
+
+    print("Nb items: {}".format(len(server._Server__indexed_dataset)))
+
+    # 1- request first index
+    res = server.get_hyper_index(index, page_size)
+    print(res)
+
+    # 2- request next index
+    print(server.get_hyper_index(res.get('next_index'), page_size))
+
+    # 3- remove the first index
+    del server._Server__indexed_dataset[res.get('index')]
+    print("Nb items: {}".format(len(server._Server__indexed_dataset)))
+
+    # 4- request again the initial index -> the first data
+    print(server.get_hyper_index(index, page_size))
+
+    # 5- request again initial next index -> same data page as the request 2-
+    print(server.get_hyper_index(res.get('next_index'), page_size))
