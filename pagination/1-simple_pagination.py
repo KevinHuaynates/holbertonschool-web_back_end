8a5792d670a4b2db6ed26dd3143725f639c15c46
KevinHuaynates <6992@holbertonstudents.com>
2024-02-04 13:41:00 -0800

    2

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+"""
+Module documentation: Simple pagination method for a dataset.
+"""
+
+import csv
+from typing import List, Optional
+from .0-simple_helper_function import index_range
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Function documentation: Returns the specified page of the dataset.
+        """
+        assert isinstance(page, int) and page > 0, "Page must be a positive integer"
+        assert isinstance(page_size, int) and page_size > 0, "Page size must be a positive integer"
+
+        dataset = self.dataset()
+
+        start_idx, end_idx = index_range(page, page_size)
+        if start_idx >= len(dataset):
+            return []  # Out of range, return empty list
+
+        return dataset[start_idx:end_idx]
+
+
+if __name__ == "__main__":
+    # Example usage
+    server = Server()
+
+    # Testing assertions
+    try:
+        should_err = server.get_page(-10, 2)
+    except AssertionError:
+        print("AssertionError raised with negative values")
+
+    try:
+        should_err = server.get_page(0, 0)
+    except AssertionError:
+        print("AssertionError raised with 0")
+
+    try:
+        should_err = server.get_page(2, 'Bob')
+    except AssertionError:
+        print("AssertionError raised when page and/or page_size are not ints")
+
+    # Print paginated results
+    print(server.get_page(1, 3))
+    print(server.get_page(3, 2))
+    print(server.get_page(3000, 100))
