e996b700405a15246402b5a9632eec5ac91f9818
KevinHuaynates <6992@holbertonstudents.com>
2024-02-08 23:21:30 -0800

    1

--- ES6_classes/9-main.js
+++ ES6_classes/9-main.js
@@ -1,6 +1,5 @@
 // En el archivo 9-hoisting.js
 
-// Declaración de la clase HolbertonClass
 export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
@@ -16,7 +15,6 @@ export class HolbertonClass {
   }
 }
 
-// Declaración de la clase StudentHolberton
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
@@ -37,16 +35,13 @@ export class StudentHolberton {
   }
 }
 
-// Instancias de las clases HolbertonClass
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-// Instancias de las clases StudentHolberton
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-// Exportación de la lista de estudiantes
 export const listOfStudents = [student1, student2, student3, student4, student5];
