bcb03b7d22d2af139de3c83082fadfa2693487ba
KevinHuaynates <6992@holbertonstudents.com>
2024-02-20 23:03:44 -0800

    9-try.js

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,22 @@
+// 9-try.js
+
+// Definir la función guardrail
+export default function guardrail(mathFunction) {
+  // Crear un array para almacenar los resultados y mensajes
+  const queue = [];
+  
+  try {
+    // Ejecutar la función mathFunction y agregar el resultado a la queue
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    // Capturar cualquier error y agregar el mensaje de error a la queue
+    queue.push(error.message);
+  } finally {
+    // Agregar el mensaje "Guardrail was processed" a la queue
+    queue.push('Guardrail was processed');
+  }
+  
+  // Retornar el array queue
+  return queue;
+}
