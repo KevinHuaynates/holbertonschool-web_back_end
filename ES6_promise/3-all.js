19602703608df7fa5c208ee76bc6115332c43f51
KevinHuaynates <6992@holbertonstudents.com>
2024-02-20 23:50:24 -0800

    4

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,24 +1,14 @@
-// 3-all.js
-
-// Importar la función uploadPhoto desde utils.js
 import { uploadPhoto, createUser } from './utils';
 
-// Definir la función handleProfileSignup
-export default async function handleProfileSignup() {
+async function handleProfileSignup() {
   try {
-    // Llamar a las funciones uploadPhoto y createUser
-    const [photoResponse, userResponse] = await Promise.all([uploadPhoto(), createUser()]);
-    
-    // Extraer los datos necesarios de las respuestas
-    const { body: photoBody } = photoResponse;
-    const { firstName, lastName } = userResponse;
-    
-    // Imprimir el resultado en la consola
-    console.log(`${photoBody} ${firstName} ${lastName}`);
+    const photo = await uploadPhoto();
+    const user = await createUser();
+
+    console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
   } catch (error) {
-    // Manejar el error si ocurre al cargar la foto
-    console.error('Signup system offline');
+    console.log('Signup system offline');
   }
 }
 
-// Eliminar los espacios en blanco al final del archivo
+export default handleProfileSignup;
