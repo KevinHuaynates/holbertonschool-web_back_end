31a3fd8e819788adb5185ab0a0a06bc55695befb
KevinHuaynates <6992@holbertonstudents.com>
2024-02-20 22:24:52 -0800

    1

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,14 @@
+// 0-promise.js
+
+// Definir la función getResponseFromAPI
+export default function getResponseFromAPI() {
+  // Devolver una nueva Promise
+  return new Promise((resolve, reject) => {
+    // Implementar la lógica para resolver la Promise
+    // Por ejemplo, puedes simular una respuesta exitosa con un tiempo de espera
+    setTimeout(() => {
+      // Resuelve la Promise con algún valor, por ejemplo, un objeto vacío
+      resolve({});
+    }, 1000); // Tiempo de espera de 1 segundo
+  });
+}
