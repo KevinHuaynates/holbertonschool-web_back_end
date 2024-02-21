4a9c7f3b9640e6e1c3076eb6c7011a752fde40bf
KevinHuaynates <6992@holbertonstudents.com>
2024-02-20 23:28:36 -0800

    4

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,24 +1,22 @@
 // 3-all.js
 
-// Importar las funciones desde utils.js
+// Importar funciones desde utils.js
 import { uploadPhoto, createUser } from './utils.js';
 
 // Definir la función handleProfileSignup
 export default function handleProfileSignup() {
-  // Crear un array para almacenar las promesas
-  const promises = [uploadPhoto(), createUser()];
-
-  // Utilizar Promise.all() para manejar múltiples promesas
-  return Promise.all(promises.map(promise =>
-    promise.catch(error => {
-      // En caso de error, logear "Signup system offline"
-      console.error("Signup system offline");
-      // Retornar una promesa rechazada para mantener el flujo
-      return Promise.reject(error);
+  // Llamar a las funciones uploadPhoto y createUser
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photoResponse, userResponse]) => {
+      // Extraer los datos necesarios de las respuestas
+      const { body: photoBody } = photoResponse;
+      const { firstName, lastName } = userResponse;
+      
+      // Imprimir el resultado en la consola
+      console.log(`${photoBody} ${firstName} ${lastName}`);
     })
-  ))
-  .then(([photoResponse, userResponse]) => {
-    // Lograr el cuerpo (body) de la respuesta de uploadPhoto y el firstName y lastName de la respuesta de createUser
-    console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
-  });
+    .catch(error => {
+      // Manejar el error si ocurre
+      console.error('Signup system offline');
+    });
 }
