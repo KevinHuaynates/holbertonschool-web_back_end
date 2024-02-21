f7076ae1fb5185db0815c3597dd0485f44826a2b
KevinHuaynates <6992@holbertonstudents.com>
2024-02-20 23:22:38 -0800

    6-final-user.js

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,5 +1,3 @@
-// 6-final-user.js
-
 // Importar funciones desde 4-user-promise.js y 5-photo-reject.js
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
@@ -13,14 +11,14 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
 
     // Esperar a que todas las promesas se completen
     await Promise.allSettled([promise1, promise2]);
-    
+
     // Retornar un array con el estado y el valor o error de cada promesa
     return [
       { status: 'fulfilled', value: { firstName, lastName } },
-      { status: 'rejected', value: `Error: ${fileName} cannot be processed` }
+      { status: 'rejected', reason: `Error: ${fileName} cannot be processed` } // Cambiado "value" a "reason" para reflejar correctamente el motivo del rechazo
     ];
   } catch (error) {
-    // Manejar cualquier error interno y lanzarlo nuevamente
+    // Lanzar el error nuevamente, no es necesario un bloque try...catch aquí
     throw error;
   }
 }
