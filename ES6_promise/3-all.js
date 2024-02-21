7be022510a3d751e55aa0aa820f6a50ff65a7cc0
KevinHuaynates <6992@holbertonstudents.com>
2024-02-20 23:30:33 -0800

    4

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -4,19 +4,19 @@
 import { uploadPhoto, createUser } from './utils.js';
 
 // Definir la función handleProfileSignup
-export default function handleProfileSignup() {
-  // Llamar a las funciones uploadPhoto y createUser
-  Promise.all([uploadPhoto(), createUser()])
-    .then(([photoResponse, userResponse]) => {
-      // Extraer los datos necesarios de las respuestas
-      const { body: photoBody } = photoResponse;
-      const { firstName, lastName } = userResponse;
-      
-      // Imprimir el resultado en la consola
-      console.log(`${photoBody} ${firstName} ${lastName}`);
-    })
-    .catch(error => {
-      // Manejar el error si ocurre
-      console.error('Signup system offline');
-    });
+export default async function handleProfileSignup() {
+  try {
+    // Llamar a las funciones uploadPhoto y createUser
+    const [photoResponse, userResponse] = await Promise.all([uploadPhoto(), createUser()]);
+    
+    // Extraer los datos necesarios de las respuestas
+    const { body: photoBody } = photoResponse;
+    const { firstName, lastName } = userResponse;
+    
+    // Imprimir el resultado en la consola
+    console.log(`${photoBody} ${firstName} ${lastName}`);
+  } catch (error) {
+    // Manejar el error si ocurre
+    console.error('Signup system offline');
+  }
 }
