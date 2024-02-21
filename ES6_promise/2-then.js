983e55e59dd418e39f42d3b8cb8900d35ea3368a
KevinHuaynates <6992@holbertonstudents.com>
2024-02-20 22:37:01 -0800

    3

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -5,12 +5,12 @@ export default function handleResponseFromAPI(promise) {
   // Retornar una nueva Promise que resuelve con un objeto que contiene status y body
   return promise.then(() => {
     // Cuando la Promise se resuelve
-    console.log("Got a response from the API");
+    console.log('Got a response from the API');
     // Devolver un objeto con status 200 y body 'success'
     return { status: 200, body: 'success' };
   }).catch(() => {
     // Cuando la Promise es rechazada
-    console.log("Got a response from the API");
+    console.log('Got a response from the API');
     // Devolver un objeto Error vacío
     return new Error();
   });
