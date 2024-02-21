b61c1bd6447c04a7f2110e5a5bb29adf43cb7932
KevinHuaynates <6992@holbertonstudents.com>
2024-02-20 23:54:51 -0800

    6-final-user.js

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,27 +1,34 @@
-// 6-final-user.js
+import { signUpUser } from './4-user-promise';
+import { uploadPhoto } from './5-photo-reject';
 
-// Importar funciones desde 4-user-promise.js y 5-photo-reject.js
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
-
-// Definir la función handleProfileSignup
 export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const result = [];
+
   try {
-    // Llamar a las funciones signUpUser y uploadPhoto y esperar a que se completen
-    const promise1 = signUpUser(firstName, lastName);
-    const promise2 = uploadPhoto(fileName);
-
-    // Esperar a que todas las promesas se completen
-    await Promise.allSettled([promise1, promise2]);
-
-    // Retornar un array con el estado y el valor o error de cada promesa
-    return [
-      { status: 'fulfilled', value: { firstName, lastName } },
-      { status: 'rejected', value: `Error: ${fileName} cannot be processed` }
-    ];
+    const user = await signUpUser(firstName, lastName);
+    result.push({
+      status: 'fulfilled',
+      value: user,
+    });
   } catch (error) {
-    // Manejar cualquier error interno y lanzarlo nuevamente
-    throw error;
+    result.push({
+      status: 'rejected',
+      value: error.toString(),
+    });
   }
-}
 
+  try {
+    const photo = await uploadPhoto(fileName);
+    result.push({
+      status: 'fulfilled',
+      value: photo,
+    });
+  } catch (error) {
+    result.push({
+      status: 'rejected',
+      value: error.toString(),
+    });
+  }
+
+  return result;
+}
