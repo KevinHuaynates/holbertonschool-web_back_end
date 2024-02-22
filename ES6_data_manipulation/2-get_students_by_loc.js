bbd120cc6f3de6a75375d6f8c5f8c4f5c37c47d9
KevinHuaynates <6992@holbertonstudents.com>
2024-02-21 21:44:29 -0800

    3

---
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,10 @@
+// Archivo: 2-get_students_by_loc.js
+
+// Definir la función getStudentsByLocation
+function getStudentsByLocation(students, city) {
+  // Utilizar el método filter para obtener los estudiantes ubicados en la ciudad especificada
+  return students.filter(student => student.location === city);
+}
+
+// Exportar la función getStudentsByLocation para que pueda ser utilizada en otros archivos
+export default getStudentsByLocation;
