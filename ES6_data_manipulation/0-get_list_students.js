c038b4edb8b9a16f2be3ff86f55e4a947018be2b
KevinHuaynates <6992@holbertonstudents.com>
2024-02-21 21:36:32 -0800

    1

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -2,12 +2,12 @@
 
 // Definir la función getListStudents
 function getListStudents() {
-    // Crear y retornar un array de objetos representando a los estudiantes
-    return [
-        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-        { id: 2, firstName: 'James', location: 'Columbia' },
-        { id: 5, firstName: 'Serena', location: 'San Francisco' }
-    ];
+  // Crear y retornar un array de objetos representando a los estudiantes
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 }
 
 // Exportar la función getListStudents para que pueda ser utilizada en otros archivos
