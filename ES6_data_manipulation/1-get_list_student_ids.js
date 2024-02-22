f35b84c5b861779a411337f0c826f60f707ef5a3
KevinHuaynates <6992@holbertonstudents.com>
2024-02-21 21:39:24 -0800

    2

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,13 +2,13 @@
 
 // Definir la función getListStudentIds
 function getListStudentIds(array) {
-    // Verificar si el argumento es un array
-    if (!Array.isArray(array)) {
-        return [];
-    }
+  // Verificar si el argumento es un array
+  if (!Array.isArray(array)) {
+    return [];
+  }
 
-    // Utilizar el método map para obtener los ids de los estudiantes
-    return array.map(student => student.id);
+  // Utilizar el método map para obtener los ids de los estudiantes
+  return array.map(student => student.id);
 }
 
 // Exportar la función getListStudentIds para que pueda ser utilizada en otros archivos
