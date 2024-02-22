4cbcf4192096cdf55669b6c0187f700cce1931ba
KevinHuaynates <6992@holbertonstudents.com>
2024-02-21 22:04:28 -0800

    8

---
+++ ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,16 @@
+// Archivo: 7-has_array_values.js
+
+// Definir la función hasValuesFromArray
+function hasValuesFromArray(set, array) {
+  // Iterar sobre cada elemento del array
+  for (let value of array) {
+    // Verificar si el elemento no está presente en el conjunto
+    if (!set.has(value)) {
+      return false; // Si no está presente, devolver falso
+    }
+  }
+  return true; // Si todos los elementos están presentes, devolver verdadero
+}
+
+// Exportar la función hasValuesFromArray para que pueda ser utilizada en otros archivos
+export default hasValuesFromArray;
