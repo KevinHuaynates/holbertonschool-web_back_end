9209f77abf567925b4a20d52646bf013c5b2fb6e
KevinHuaynates <6992@holbertonstudents.com>
2024-02-21 22:16:34 -0800

    9

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,18 @@
+// Archivo: 9-groceries_list.js
+
+// Definir la función groceriesList
+function groceriesList() {
+  // Crear un nuevo mapa (Map) con los artículos de la lista de compras y sus cantidades
+  const groceries = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5]
+  ]);
+
+  return groceries;
+}
+
+// Exportar la función groceriesList para que pueda ser utilizada en otros archivos
+export default groceriesList;
