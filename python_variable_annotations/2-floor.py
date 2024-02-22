d93c3a467453ad333c9928cc26dde2e9d0c90f82
KevinHuaynates <6992@holbertonstudents.com>
2024-02-21 22:47:48 -0800

    3

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+Module to define a function to get the floor of a float.
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Get the floor of a float.
+
+    Args:
+        n (float): The input float number.
+
+    Returns:
+        int: The floor of the input float.
+    """
+    return math.floor(n)
