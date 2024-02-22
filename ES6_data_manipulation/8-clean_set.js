72be9ab1aae96dea0c86db6bf057f6f0f52bb132
KevinHuaynates <6992@holbertonstudents.com>
2024-02-21 22:14:35 -0800

    9

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -7,21 +7,26 @@ function cleanSet(set, startString = '') {
     return '';
   }
 
+  // Verificar si no se proporciona startString
+  if (startString === '') {
+    // Si no se proporciona startString, unir los elementos del conjunto con "-" y devolver la cadena resultante
+    return Array.from(set).join("-");
+  }
+
   // Inicializar una matriz vacía para almacenar los valores que coinciden con el startString
   const matches = [];
 
   // Iterar sobre cada elemento del conjunto
   for (const value of set) {
-    // Verificar si el valor comienza con el startString o si startString está vacío
+    // Verificar si el valor comienza con el startString
     if (value.startsWith(startString)) {
       // Si coincide, agregar el valor (sin startString) a la matriz matches
       matches.push(value.slice(startString.length));
     }
   }
 
-  // Si no se proporciona startString, unir los elementos del conjunto con "-" y devolver la cadena resultante
-  // De lo contrario, unir los elementos de la matriz matches con "-" y devolver la cadena resultante
-  return startString === '' ? Array.from(set).join("-") : matches.join("-");
+  // Unir los elementos de la matriz matches con "-" y devolver la cadena resultante
+  return matches.join("-");
 }
 
 // Exportar la función cleanSet para que pueda ser utilizada en otros archivos
