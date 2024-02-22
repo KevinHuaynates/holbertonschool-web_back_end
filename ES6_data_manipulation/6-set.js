f223e0da83f7336fde45bcb8a506c75a1bf7414a
KevinHuaynates <6992@holbertonstudents.com>
2024-02-21 22:02:46 -0800

    7

---
+++ ES6_data_manipulation/6-set.js
@@ -0,0 +1,10 @@
+// Archivo: 6-set.js
+
+// Definir la función setFromArray
+function setFromArray(array) {
+  // Crear un nuevo Set a partir del array proporcionado
+  return new Set(array);
+}
+
+// Exportar la función setFromArray para que pueda ser utilizada en otros archivos
+export default setFromArray;
