c08b1b5479ea1f62acce8161ad7ae3a4509d92ed
KevinHuaynates <6992@holbertonstudents.com>
2024-03-03 17:31:12 -0800

    1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""
+Module to define a function to get the floor of a float.
+"""
 from typing import List
 import asyncio
 
@@ -6,7 +9,7 @@ from async_generator import async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """Collects 10 random numbers using an async comprehensing over async_generator.
+    """Collects 10 random numbers using an async comprehensing.
 
     Returns:
         List of 10 random numbers.
