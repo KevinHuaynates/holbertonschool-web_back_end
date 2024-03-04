75e37e05150194164aebc03cdf8d663c300510ae
KevinHuaynates <6992@holbertonstudents.com>
2024-03-03 17:03:18 -0800

    1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python3
-"""
-This module defines a coroutine that collects 10 random numbers.
-"""
 
-import asyncio
 from typing import List
-from random import uniform
-from itertools import islice
 
-async_generator = __import__('0-async_generator').async_generator
+from . import async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """Coroutine that collects 10 random numbers"""
-    return [num async for num in islice(async_generator(), 10)]
-
-
+    """
+    This coroutine collects 10 random numbers using an async.
+    """
+    return [number async for number in async_generator()]
