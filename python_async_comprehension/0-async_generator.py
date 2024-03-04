c3dc2b2119cfbb87bfd93e7727d6bfb23385f3b4
KevinHuaynates <6992@holbertonstudents.com>
2024-03-03 17:11:48 -0800

    !

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,14 +2,17 @@
 """
 Module to define a function to get the floor of a float.
 """
+from typing import Generator
 import asyncio
 import random
 
 
-async def async_generator():
-    """
-    This generator yields random numbers between 0 and 10, asynchronously.
+async def async_generator() -> Generator[float, None, None]:
+    """Generator that yields a random number between 0 and 10.
+
+    Yields:
+        Random number between 0 and 10.
     """
     for _ in range(10):
-        await asyncio.sleep(1)  # Yield control
-        yield random.randint(0, 10)
+        await asyncio.sleep(1)
+        yield random.random() * 10
